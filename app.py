7421a963a268049d796bf3352e5af3922c4eb9cc
Tirth Patel <67674585+Tirth132108@users.noreply.github.com>
2022-11-03 13:36:59 -0400

    Add files via upload
    An update regarding the database connection.

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
+#prepared by : Tirth Patel(1206441) and Niyatiben Salot(1206446)
+
+from colorama import Cursor
 import pandas as pd
 import numpy as np
 import sklearn
+from flaskext.mysql import MySQL
+import mysql.connector
 import joblib
 from sklearn.preprocessing import FunctionTransformer, MinMaxScaler
 from sklearn.exceptions import NotFittedError
@@ -14,10 +19,29 @@ logging.basicConfig(level=logging.WARN)
 logger = logging.getLogger(__name__)
 import dvc.api
 from flask import Flask, render_template, redirect, request
+
+import mysql.connector
+
 app = Flask(__name__)
 
-columns = ['Name','Location','Year','Kilometers_Driven','Fuel_Type','Transmission','Owner_Type','Mileage','Engine','Power','Seats']
 
+ 
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd="",
+  database="Car_Prediction"
+)
+
+
+#specifiying the features name and their key's values
+columns = ['Name','Location','Year','Kilometers_Driven','Fuel_Type','Transmission','Owner_Type','Mileage','Engine','Power','Seats']
+locations = ['Mumbai', 'Pune', 'Chennai', 'Coimbatore', 'Hyderabad', 'Jaipur',
+       'Kochi', 'Kolkata', 'Delhi', 'Bangalore', 'Ahmedabad']
+
+
+#loading pipeline that has been created
 model = joblib.load("ml_pipeline") 
 
 @app.route("/")
@@ -28,6 +52,7 @@ def hello_world():
 @app.route("/submit", methods =['POST', 'GET'])
 def data():
     if request.method == 'POST':
+        #reading the data 
         Name = request.form['Name']
         Location = request.form['Location']
         Year = int(request.form['Year'])
@@ -38,16 +63,26 @@ def data():
         Mileage = request.form['Mileage']
         Engine = str(request.form['Engine'])
         Seats = request.form['Seats']
-        Engine = Engine.split(' ')
+        Engine = Engine.split(' ') #Engine has values like 67 cc, separating 67
         Engine = Engine[0]
         Engine = float(Engine)
         current_year = 2022
-        Age = current_year - Year
+        Age = current_year - Year # calculating new feature AGE
+
+        cursor = mydb.cursor(dictionary=True, buffered=True)
+    
+        cursor.execute(''' INSERT INTO data (Name,Location,Year,Kilometers_Driven,Fuel_Type,Transmission,Owner_Type,Mileage,Engine,Seats) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)''',[Name,Location,Year,Kilometers_Driven,Fuel_Type,Transmission,Owner_Type,Mileage,Engine,Seats])
+        mydb.commit()
+
         features = [[Location,Age,Kilometers_Driven,Fuel_Type,Transmission,Owner_Type,Mileage,Engine,Seats]]
+        # creating dataframe
         df = pd.DataFrame(features, columns=['Location', 'Age','Kilometers_Driven','Fuel_Type','Transmission','Owner_Type','Mileage','Engine','Seats'])
+        
+        #making prediction
         prediction = model.predict(df)
-        prediction = round(np.exp(prediction[0]),2)
-        print(prediction)
+
+        prediction = round(np.exp(prediction[0]),2) #having 2 numbers after decimal. 
+       
 
     return render_template("index.html", prediction=prediction)
 
@@ -56,5 +91,5 @@ def data():
 
 if __name__ =='__main__':
 	#app.debug = True
-	app.run(debug = True,host='0.0.0.0', port=80)
+	app.run(debug = True)
 
